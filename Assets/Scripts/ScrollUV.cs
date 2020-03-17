aab43637240376cc0fcf6d2f41f5964a04e37141
kmazurek96 <konradmazurek12@gmail.com>
2020-03-17 14:13:21 +0100

    StartMenu,SceneLoader

---
+++ Assets/Scripts/ScrollUV.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollUV : MonoBehaviour
+{
+    [SerializeField] float scrollingSpeed;
+
+    void Update()
+    {
+        MeshRenderer ar = GetComponent<MeshRenderer>();
+
+        Material mat = ar.material;
+
+        mat.mainTexture.wrapMode = TextureWrapMode.Repeat;
+
+        Vector2 offset = mat.mainTextureOffset;
+
+        offset.y += Time.deltaTime/ scrollingSpeed;
+
+        mat.mainTextureOffset = offset;
+    }
+}

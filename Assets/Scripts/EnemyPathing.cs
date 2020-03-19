5c0205491c7892d73533cd3e009717e9d6a8c787
unknown <michaloxs123@gmail.com>
2020-03-19 15:53:00 +0100

    przeciwnik

---
+++ Assets/Scripts/EnemyPathing.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPathing : MonoBehaviour
+{
+    [SerializeField] List<Transform> wayPoints;
+    [SerializeField] float moveSpeed = 2f;
+    int waypointIndex = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = wayPoints[waypointIndex].transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(waypointIndex <= wayPoints.Count - 1)
+        {
+            var targetPosition = wayPoints[waypointIndex].transform.position;
+            var movementThisFrame = moveSpeed * Time.deltaTime;
+            this.transform.position = Vector3.MoveTowards(transform.position, targetPosition, movementThisFrame);
+            Debug.Log(targetPosition);
+            if (transform.position == targetPosition)
+            {
+                waypointIndex++;
+                Debug.Log(waypointIndex);
+            }
+        }
+        else
+        {
+            waypointIndex = 0;
+        }
+    }
+}

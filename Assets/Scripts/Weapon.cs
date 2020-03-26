1d061a778800bb84480aaf0ad4674893288611b5
kmazurek96 <konradmazurek12@gmail.com>
2020-03-26 12:38:19 +0100

    Shooting part1

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public Transform firePoint;
+    public GameObject bulletPrefab;
+    public int ammo = 5;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if(Input.GetButtonDown("Fire1")&& ammo!=0)
+        {
+            Shoot();
+        }
+
+
+    }
+
+    void Shoot()
+    {
+        Instantiate(bulletPrefab, firePoint.transform.position, firePoint.rotation);
+        ammo--;
+    }
+}

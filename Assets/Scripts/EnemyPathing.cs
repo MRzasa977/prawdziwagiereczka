dc65d448cd1aa1e7c3e2c534d142851fad605871
unknown <michaloxs123@gmail.com>
2020-04-14 15:55:35 +0200

    Przeciwnicy itp

--- Assets/Scripts/EnemyPathing.cs
+++ Assets/Scripts/EnemyPathing.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class EnemyPathing : MonoBehaviour
 {
+    [SerializeField] WaveConfig waveConfig;
     [SerializeField] List<Transform> wayPoints;
     [SerializeField] float moveSpeed = 2f;
     int waypointIndex = 0;
     // Start is called before the first frame update
     void Start()
     {
+        wayPoints = waveConfig.GetWayPoints();
         transform.position = wayPoints[waypointIndex].transform.position;
     }
 

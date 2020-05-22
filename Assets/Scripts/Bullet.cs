5c2b443ee4748a2bbfe26938628236afd87e949a
unknown <michaloxs123@gmail.com>
2020-05-22 13:13:49 +0200

    Zabijanie

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -6,10 +6,23 @@ public class Bullet : MonoBehaviour
 {
     public float speed = 20f;
     public Rigidbody2D rb;
+
+    private EnemyPathing enemyPathing;
     void Start()
     {
         rb.velocity = transform.up * speed;
     }
 
-   
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Enemy")
+        {
+            EnemyPathing enemyPathing = collision.GetComponent<EnemyPathing>();
+            Destroy(gameObject);
+            Destroy(enemyPathing.gameObject);
+            
+        }
+    }
+
+
 }
